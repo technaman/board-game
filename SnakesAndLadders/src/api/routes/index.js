0b4d3fb92d241e67ccfc96c670ae5eb7259e3a64
Naman Garg <naman.garg@jarvis.consulting>
2022-02-03 16:55:09 +0530

    added home route

--- SnakesAndLadders/src/api/routes/index.js
+++ SnakesAndLadders/src/api/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+app.get('/', (req, res) => {
+    res.send('Under Development. Please stay tuned !!');
+});
+
 router.post('/login', (req, res) => {
     res.send('Login successful');
 });
